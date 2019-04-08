f5b0bc63690a37f5399b4842d44b08ce552151e7
Bhumesh Domala <bhumesh@codeft.in>
2019-04-08 19:30:33 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,51 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    words: null,
+  }
+  fetchWords = e => {
+    fetch(`https://www.vocabulary.com/dictionary/autocomplete?search=${e.target.value}`)
+      .then(res => res.text())
+      .then(data => {
+        let div = document.createElement('div');
+        div.innerHTML = data;
+        const words = div.innerText.split('\n').filter(Boolean).map(wordData => {
+          let wordArray = wordData.split(' ');
+          return {
+            name: wordArray.shift(),
+            description: wordArray.join(' ')
+          }
+        });
+        this.setState({
+          words
+        }, () => {
+          div = null;
+        })
+      })
+  }
+
+  fetchWord = word => {
+    fetch(`https://www.vocabulary.com/dictionary/definition.ajax?search=${word}&lang=en`)
+      .then(res => res.text())
+      .then(data => {
+        let div = document.createElement('div');
+        console.log(data);
+      }) 
+  }
   render() {
+    const { words } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Dictionary</h1>
+        <input type="text" onChange={this.fetchWords} />
+        {
+          words && <ul>
+            {
+              words.map((word, id)=> <li key={id} onClick={() => {this.fetchWord(word.name)}}><strong>{word.name}: </strong>{word.description}</li>)
+            }
+          </ul>
+        }
       </div>
     );
   }
