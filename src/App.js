068a7d786fa0daf3cd516828e0c7bc40b7874131
Bhumesh Domala <bhumesh.domala@qvantel.com>
2020-11-03 15:30:23 +0530

    changed API url to proper one, added some stylings

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
+const API_URL = "https://vocabulary.now.sh"
 
 class App extends Component {
   state = {
     words: null,
     word: null,
-    loading: false
+    loading: false,
+    search: "",
   };
   fetchWords = e => {
     if (!e.target.value) return;
-    fetch(`http://35.200.150.228:7778/words/${e.target.value}`)
+    fetch(`${API_URL}/words/${e.target.value}`)
       .then(res => res.json())
       .then(res => {
         if (res.success) {
           this.setState({
-            words: res.data.slice(0, 7)
+            words: res.data
           });
         }
       });
@@ -24,7 +26,7 @@ class App extends Component {
       loading: true
     });
 
-    fetch(`http://35.200.150.228:7778/word/${word}`)
+    fetch(`${API_URL}/word/${word}`)
       .then(res => res.json())
       .then(res => {
         if (res.success) {
@@ -39,17 +41,29 @@ class App extends Component {
         }
       });
   };
+
+  handleInputChange = (e) => {
+    if (!e.target.value) return;
+    this.setState({
+      search: e.target.value,
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   render() {
     const { words, word, loading } = this.state;
     return (
       <div className="container">
         <header>
           <h2>Dictionary</h2>
-          <form>
-            <input type="text" onChange={this.fetchWords} placeholder="search..." autoFocus />
-          </form>
         </header>
-        {words && (
+        <main>
+        <form onSubmit={this.handleSubmit}>
+            <input type="text" onChange={this.fetchWords} placeholder="search..." autoFocus />
+            {words && (
           <ul>
             {words.map((word, id) => (
               <li
@@ -64,6 +78,8 @@ class App extends Component {
             ))}
           </ul>
         )}
+          </form>
+        </main>
         {word && (
           <div className="word-wrapper">
             <h1>{word.name}</h1>
